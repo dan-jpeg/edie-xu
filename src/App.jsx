6f0681971cca36cb1443b4da774107667dc6965c
dan <dancr.wley@gmail.com>
2024-07-31 23:14:10 -0400

    tweakinnn

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,6 @@ import './App.css';
 const projects = [
     {
         id: "1",
-        title: "0 20 24 ",
-        description: "sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.",
-        material: "stoneware",
-        year: "2024",
-        images: ["https://i.ibb.co/DLZRPMf/PNG-image-3.jpg", "https://i.ibb.co/93ZqQmn/PNG-image-1.jpg", "https://i.ibb.co/tHRbPhC/PNG-image-2.jpg"],
-    },
-    {
-        id: "2",
-        title: "oxygenated  ",
-        description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        material: "stoneware",
-        year: "2024",
-        images: ["https://i.ibb.co/1n9KFhH/183-A0937-Finished-Crop-2.jpg", "https://i.ibb.co/Z8FnjyZ/1-Artboard-1.png", "https://i.ibb.co/ygRym71/PNG-image.jpg", "https://i.ibb.co/zNvMFWS/183-A0940-Finished.jpg"],
-    },
-    {
-        id: "3",
         category: "Group Exhibition",
         location: "Shanghart, ShangHai",
         title: "limited intentionality",
@@ -56,6 +40,27 @@ const projects = [
         year: "2024",
         images: ["https://i.ibb.co/1n9KFhH/183-A0937-Finished-Crop-2.jpg", "https://i.ibb.co/Z8FnjyZ/1-Artboard-1.png", "https://i.ibb.co/ygRym71/PNG-image.jpg", "https://i.ibb.co/zNvMFWS/183-A0940-Finished.jpg"],
     },
+    {
+        id: "2",
+        category: " ",
+        location: " ",
+        title: "0 20 24 ",
+        description: "sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.",
+        material: "stoneware",
+        year: "2024",
+        images: ["https://i.ibb.co/DLZRPMf/PNG-image-3.jpg", "https://i.ibb.co/93ZqQmn/PNG-image-1.jpg", "https://i.ibb.co/tHRbPhC/PNG-image-2.jpg"],
+    },
+    {
+        id: "3",
+        category: " ",
+        location: " ",
+        title: "oxygenated  ",
+        description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        material: "stoneware",
+        year: "2024",
+        images: ["https://i.ibb.co/1n9KFhH/183-A0937-Finished-Crop-2.jpg", "https://i.ibb.co/Z8FnjyZ/1-Artboard-1.png", "https://i.ibb.co/ygRym71/PNG-image.jpg", "https://i.ibb.co/zNvMFWS/183-A0940-Finished.jpg"],
+    },
+
 
 ];
 
@@ -80,7 +85,7 @@ const App = () => {
     return (
         <div className="app-container">
             <aside className={`sidebar ${sideBarExpanded ? 'expanded' : 'collapsed'}`}>
-                <h3>edie xu</h3>
+                <h3 onClick={handleWorksClick}>edie xu</h3>
                 <h3 className="clickable" onClick={handleWorksClick}>selected works</h3>
                 <ul>
                     {sideBarExpanded && projects.map((project) => (
@@ -113,6 +118,8 @@ const ProjectListing = ({ project, onProjectClick }) => {
             <div className="title-container">
                 <p className="listing-title">{project.title}</p>
                 <p>{project.material}</p>
+                <p>{project.category}</p>
+                <p>{project.location}</p>
             </div>
             <div className="image-container">
                 <img className="project-image" src={project.images[0]} alt={project.title} />
@@ -127,10 +134,11 @@ const DisplayedProject = ({ project }) => {
             <div className="title-container">
                 <p>{project.title}</p>
                 <p>{project.material}</p>
-
+                <p>{project.category}</p>
+                <p>{project.location}</p>
             </div>
             <div className="image-container">
-                <p>{project.description}</p>
+                <p >{project.description}</p>
                 {project.images.map((image, index) => (
                     <img key={index} className="project-image" src={image} alt={`${project.title} ${index}`}/>
                 ))}
@@ -144,6 +152,8 @@ ProjectListing.propTypes = {
         title: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
         material: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        location: PropTypes.string.isRequired,
         images: PropTypes.arrayOf(PropTypes.string).isRequired
     }).isRequired,
     onProjectClick: PropTypes.func.isRequired,
@@ -154,8 +164,9 @@ DisplayedProject.propTypes = {
         title: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
         material: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        location: PropTypes.string.isRequired,
         images: PropTypes.arrayOf(PropTypes.string).isRequired
     }).isRequired
 };
-
 export default App;
