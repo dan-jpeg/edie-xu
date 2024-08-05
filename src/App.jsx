8dd05c67aa68a3ffb58866309d2ad9281de1b06e
dan <dancr.wley@gmail.com>
2024-08-05 00:38:20 -0400

    content update

--- src/App.jsx
+++ src/App.jsx
@@ -4,140 +4,223 @@ import './App.css';
 
 const projects = [
     {
+        id: "0",
+        category: " ",
+        location: " ",
+        title: "CMMMMC - Ceramic Meets Metal Metal Meets Ceramic",
+        description: "Ceramic Meets Metal Metal Meets Ceramic",
+        material: "Oxidized metal and broken fired stoneware",
+        year: "2024",
+        dimensions: "70 x 23 x 94 inches (177 x 58 x 240 cm)",
+        duration: "15 min",
+        images: ["https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/ceramic_metal_01.png", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/ceramic_metal_02.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/ceramic_metal_03.JPG"]
+    },
+    {
         id: "1",
         category: "Group Exhibition",
         location: "Shanghart, ShangHai",
         title: "limited intentionality",
-
-        description: "if, as kant claimed, schematization is the very mechanism for human beings’ use of intelligence, then vision can be considered apremise for human reason.  edie xu‘s works reveal the effect of sexuality on intentional activities, where the relation between body schema and eroticism always implies the object-oriented dimension.",
+        description: "if, as kant claimed, schematization is the very mechanism for human beings' use of intelligence, then vision can be considered apremise for human reason.  edie xu's works reveal the effect of sexuality on intentional activities, where the relation between body schema and eroticism always implies the object-oriented dimension.",
         material: "stoneware",
         year: "2024",
-        images: ["https://i.ibb.co/5WSgCmG/BNL3762-copy-2.jpg","https://i.ibb.co/fnFLDGg/BNL3767-copy-2.jpg", "https://i.ibb.co/5WSgCmG/BNL3762-copy-2.jpg"]
+        images: ["https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/limited_intentionality_01.jpeg","https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/limited_intentionality_02.jpeg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/limited_intentionality_03.jpeg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/limited_intentionality_04.jpeg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/limited_intentionality_05.jpeg"]
     },
     {
         id: "2",
         category: " ",
         location: " ",
         title: "0 20 24 ",
-        description: "sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.",
+        description: "",
         material: "stoneware",
-        year: "2024",
+        year: "2023",
         images: ["https://i.ibb.co/DLZRPMf/PNG-image-3.jpg", "https://i.ibb.co/93ZqQmn/PNG-image-1.jpg", "https://i.ibb.co/tHRbPhC/PNG-image-2.jpg"],
     },
     {
         id: "3",
         category: " ",
         location: " ",
-        title: "oxygenated  ",
-        description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        material: "stoneware",
-        year: "2024",
-        images: ["https://i.ibb.co/1n9KFhH/183-A0937-Finished-Crop-2.jpg", "https://i.ibb.co/Z8FnjyZ/1-Artboard-1.png", "https://i.ibb.co/ygRym71/PNG-image.jpg", "https://i.ibb.co/zNvMFWS/183-A0940-Finished.jpg"],
+        title: "Thine air",
+        description: "An unglazed stoneware piece created in 2023",
+        material: "Unglazed stoneware",
+        year: "2023",
+        dimensions: "34 x 23 x 15 inches (86 x 58 x 38 cm)",
+        images: ["https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/thine_air_01.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/thine_air_02.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/thine_air_03.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/thine_air_04.jpg"]
     },
 
     {
         id: "4",
         category: " ",
         location: " ",
-        title: "allure of the abject",
-        description: "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        title: "oxygenated  ",
+        description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         material: "stoneware",
         year: "2024",
-        images:  [
-            "https://i.ibb.co/HVDq0gg/1.png",
-            "https://i.ibb.co/HBN8Y8S/2.jpg",
-            "https://i.ibb.co/SKvM3pH/IMG-0136-copy.jpg",
-            "https://i.ibb.co/JF225xv/IMG-0219-copy.jpg",
-            "https://i.ibb.co/Bn3CK7D/IMG-0240-copy.jpg",
-            "https://i.ibb.co/BfnmSDx/IMG-0324-copy.jpg",
-            "https://i.ibb.co/qJfLttx/IMG-9961-copy.jpg",
-            "https://i.ibb.co/d0BcdQ9/Screenshot-2024-07-31-at-7-11-20-PM.png",
-            "https://i.ibb.co/y8hYf0q/Screenshot-2024-07-31-at-7-33-39-PM.png"
-        ],
+        dimensions: "34 x 23 x 15 inches (86 x 58 x 38 cm)",
+        images: ["https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/oxygenated_01.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/oxygenated_02.png", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/oxygenated_03.png", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/oxygenated_04.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/oxygenated_05.jpg"],
     },
     {
         id: "5",
         category: " ",
         location: " ",
-        title: "N...S...",
-        description: "consectet cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        material: "stoneware",
-        year: "2024",
+        title: "philtrum",
+        description: "",
+        material: "stoneware with wash",
+        year: "2022",
         images:  [
-            "https://i.ibb.co/zPz1y7f/1.png",
-            "https://i.ibb.co/QnjpLcL/Screenshot-2024-07-31-at-7-13-40-PM.png",
-            "https://i.ibb.co/vLscbV7/Screenshot-2024-08-01-at-7-09-44-PM.png"
+            "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/philtrum_01.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/philtrum_02.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/philtrum_03.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/philtrum_04.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/philtrum_05.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/philtrum_06.jpg"
         ],
     },
+    // {
+    //     id: "5",
+    //     category: " ",
+    //     location: " ",
+    //     title: "N...S...",
+    //     description: "consectet cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    //     material: "stoneware",
+    //     year: "2024",
+    //     images:  [
+    //         "https://i.ibb.co/zPz1y7f/1.png",
+    //         "https://i.ibb.co/QnjpLcL/Screenshot-2024-07-31-at-7-13-40-PM.png",
+    //         "https://i.ibb.co/vLscbV7/Screenshot-2024-08-01-at-7-09-44-PM.png"
+    //     ],
+    // },
 
     {
         id: "6",
         category: " ",
         location: " ",
-        title: "Out of place",
-        description: "consectet cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        material: "stoneware",
-        year: "2024",
-        images:  [
-            "https://i.ibb.co/16J3rRD/1-V6-A8863-copy.jpg",
-            "https://i.ibb.co/6bXvn8W/1-V6-A8872-copy.jpg",
-            "https://i.ibb.co/4T0hLLw/1-V6-A8924-copy-2.jpg",
-            "https://i.ibb.co/FX9YQ0p/1-V6-A8962-copy.jpg",
-            "https://i.ibb.co/8s3FPvY/3-L7-A6632-copy.jpg",
-            "https://i.ibb.co/n6hbxWD/3-L7-A6633-copy.jpg",
-            "https://i.ibb.co/fD09Nk8/3-L7-A8971-copy.jpg",
-            "https://i.ibb.co/Tbkm63M/3-L7-A8974-copy.jpg",
-            "https://i.ibb.co/71KxxPS/3-L7-A8979-copy.jpg",
-            "https://i.ibb.co/LxCmkYV/3-L7-A8981-copy.jpg",
-            "https://i.ibb.co/RY133Ns/Wechat-IMG221-copy.jpg",
-            "https://i.ibb.co/V3vVrBK/Wechat-IMG223-copy-2.jpg"
-        ],
+        title: "Being series 01",
+        description: "An unglazed stoneware piece created in 2022",
+        material: "Unglazed stoneware",
+        year: "2022",
+        dimensions: "24 x 18 x 15 inches (60 x 45 x 38 cm)",
+        images: ["https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/being-01_01.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/being-01_02.png"]
     },
-
     {
         id: "7",
         category: " ",
         location: " ",
-        title: "Resonate with fragmentation",
-        description: "consectet cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        material: "stoneware",
-        year: "2024",
-        images: [
-            "https://i.ibb.co/dr91b9L/01-copy.jpg",
-            "https://i.ibb.co/6X8ykWx/02-copy.jpg",
-            "https://i.ibb.co/8z4zx7R/03-copy.jpg",
-            "https://i.ibb.co/89ZDKZ7/04-copy.png"
-        ]
+        title: "Being series 02",
+        description: "A terracotta piece created in 2023",
+        material: "Terracotta",
+        year: "2023",
+        dimensions: "35 x 30 inches (89 x 76 cm)",
+        images: ["https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/being-02_02.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/being-02_02.jpg"]
     },
-
     {
-        id: "7",
+        id: "8",
         category: " ",
         location: " ",
-        title: "Somatic Attunement",
-        description: "consectet cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        material: "stoneware fired with greenware",
-        year: "2024",
-        images: [
-            "https://i.ibb.co/vJx0d4N/3L7A6311.jpg",
-            "https://i.ibb.co/swgvLrP/3L7A6383.jpg",
-            "https://i.ibb.co/ZXJb8sZ/3L7A6402.jpg",
-            "https://i.ibb.co/P16C1fg/3L7A6441.jpg",
-            "https://i.ibb.co/HzCQbgj/3L7A6517.jpg",
-            "https://i.ibb.co/FXkR815/3L7A6586.jpg",
-            "https://i.ibb.co/JmxrkBW/3L7A6591.jpg",
-            "https://i.ibb.co/DRFTQLp/3L7A6594.jpg",
-            "https://i.ibb.co/tHGJqLS/3L7A6601.jpg",
-            "https://i.ibb.co/LNJCyFG/3L7A6656.jpg",
-            "https://i.ibb.co/FgJmXJQ/DSC02337-1-copy.jpg"
-        ]
+        title: "White (resonance)",
+        description: "A stoneware piece with glaze created in 2022",
+        material: "Stoneware with glaze",
+        year: "2022",
+        dimensions: "48 x 24 x 32 inches (121 x 60 x 81 cm)",
+        images: ["https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/white_resonance_01.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/white_resonance_02.png", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/white_resonance_03.png"]
     },
+    {
+        id: "9",
+        category: " ",
+        location: " ",
+        title: "Peculiar intimacy",
+        description: "A mixed media piece created in 2022",
+        material: "Machine knit, metal and plaster",
+        year: "2022",
+        dimensions: "75 x 65 x 28 inches (190 x 165 x 71 cm)",
+        images: ["https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/peculiar_intimacy_01.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/peculiar_intimacy_02.jpg", "https://edie-xu-portfolio.s3.us-east-2.amazonaws.com/photos/peculiar_intimacy_03.jpg"]
+    },
+    // {
+    //     id: "6",
+    //     category: " ",
+    //     location: " ",
+    //     title: "Out of place",
+    //     description: "consectet cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    //     material: "stoneware",
+    //     year: "2024",
+    //     images:  [
+    //         "https://i.ibb.co/16J3rRD/1-V6-A8863-copy.jpg",
+    //         "https://i.ibb.co/6bXvn8W/1-V6-A8872-copy.jpg",
+    //         "https://i.ibb.co/4T0hLLw/1-V6-A8924-copy-2.jpg",
+    //         "https://i.ibb.co/FX9YQ0p/1-V6-A8962-copy.jpg",
+    //         "https://i.ibb.co/8s3FPvY/3-L7-A6632-copy.jpg",
+    //         "https://i.ibb.co/n6hbxWD/3-L7-A6633-copy.jpg",
+    //         "https://i.ibb.co/fD09Nk8/3-L7-A8971-copy.jpg",
+    //         "https://i.ibb.co/Tbkm63M/3-L7-A8974-copy.jpg",
+    //         "https://i.ibb.co/71KxxPS/3-L7-A8979-copy.jpg",
+    //         "https://i.ibb.co/LxCmkYV/3-L7-A8981-copy.jpg",
+    //         "https://i.ibb.co/RY133Ns/Wechat-IMG221-copy.jpg",
+    //         "https://i.ibb.co/V3vVrBK/Wechat-IMG223-copy-2.jpg"
+    //     ],
+    // },
+    // {
+    //     id: "7",
+    //     category: " ",
+    //     location: " ",
+    //     title: "Resonate with fragmentation",
+    //     description: "consectet cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    //     material: "stoneware",
+    //     year: "2024",
+    //     images: [
+    //         "https://i.ibb.co/dr91b9L/01-copy.jpg",
+    //         "https://i.ibb.co/6X8ykWx/02-copy.jpg",
+    //         "https://i.ibb.co/8z4zx7R/03-copy.jpg",
+    //         "https://i.ibb.co/89ZDKZ7/04-copy.png"
+    //     ]
+    // },
+    // {
+    //     id: "8",
+    //     category: " ",
+    //     location: " ",
+    //     title: "Somatic Attunement",
+    //     description: "consectet cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    //     material: "stoneware fired with greenware",
+    //     year: "2024",
+    //     images: [
+    //         "https://i.ibb.co/vJx0d4N/3L7A6311.jpg",
+    //         "https://i.ibb.co/swgvLrP/3L7A6383.jpg",
+    //         "https://i.ibb.co/ZXJb8sZ/3L7A6402.jpg",
+    //         "https://i.ibb.co/P16C1fg/3L7A6441.jpg",
+    //         "https://i.ibb.co/HzCQbgj/3L7A6517.jpg",
+    //         "https://i.ibb.co/FXkR815/3L7A6586.jpg",
+    //         "https://i.ibb.co/JmxrkBW/3L7A6591.jpg",
+    //         "https://i.ibb.co/DRFTQLp/3L7A6594.jpg",
+    //         "https://i.ibb.co/tHGJqLS/3L7A6601.jpg",
+    //         "https://i.ibb.co/LNJCyFG/3L7A6656.jpg",
+    //         "https://i.ibb.co/FgJmXJQ/DSC02337-1-copy.jpg"
+    //     ]
+    // },
 
-
-
-
+    // {
+    //     id: "10",
+    //     category: " ",
+    //     location: " ",
+    //     title: "02 20",
+    //     description: "A stoneware piece created in 2023",
+    //     material: "Stoneware",
+    //     year: "2023",
+    //     dimensions: "12 x 12 x 27 inches (30 x 30 x 69 cm)",
+    //     images: ["placeholder1.jpg", "placeholder2.jpg"]
+    // },
+    //
+    // {
+    //     id: "14",
+    //     category: " ",
+    //     location: " ",
+    //     title: "Being series 02",
+    //     description: "A terracotta piece created in 2023",
+    //     material: "Terracotta",
+    //     year: "2023",
+    //     dimensions: "35 x 30 inches (89 x 76 cm)",
+    //     images: ["placeholder1.jpg", "placeholder2.jpg"]
+    // },
 
 ];
+
+
+
+
+
+
 
 const App = () => {
     const [selectedProject, setSelectedProject] = useState(null);
